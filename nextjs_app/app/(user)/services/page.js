ebb7807f391a931ccb39bc01aecab0e333b1fc4d
Ansh Gupta <ng607301@gmail.com>
2026-03-24 16:20:03 +0530

    image component

--- nextjs_app/app/(user)/services/page.js
+++ nextjs_app/app/(user)/services/page.js
@@ -1,6 +1,38 @@
+import Image from "next/image"
 const Services = ()=>{
     return(
-        <h1>hello All services</h1>
+        <>
+        <h1>Our Services</h1>
+        <div className="grid grid-cols-3 ">
+        {/* service card 1*/}
+        <div className="w-60 h-60 bg-white p-6 rounded-lg">
+            {/* image box */}
+            <div className="w-24 h-24 border-2 rounded-full flex justify-between items-center mx-auto mb-4 bg-blue-400">
+                <Image src='/web.jpg' width={500} height={500} alt="web" className="w-full h-full rounded-full"/>
+            </div>
+            <h3 className="text-black">Web Development</h3>
+            <p className="text-black">Our main sevice the web devlopment we develop the website for the brands.</p>
+        </div>
+{/* service card 2 */}
+        <div className="w-60 h-60 bg-white p-6 rounded-lg">
+            {/* image box */}
+            <div className="w-full h-full border-2 rounded-full flex justify-between items-center mx-auto mb-4 bg-blue-400 relative">
+                <Image src='/web.jpg' alt="web" fill={true}/>
+            </div>
+            <h3 className="text-black">Social media marketing</h3>
+            <p className="text-black">Our main sevice the web devlopment we develop the website for the brands.</p>
+        </div>
+{/* {service card 3} */}
+        <div className="w-60 h-60 bg-white p-6 rounded-lg">
+            {/* image box */}
+            <div className="w-24 h-24 border-2 rounded-full flex justify-between items-center mx-auto mb-4 bg-blue-400">
+                <span className=" text-2xl font-bold text-purple-500">VBT</span>
+            </div>
+            <h3 className="text-black">Digital marketing</h3>
+            <p className="text-black">Our main sevice the web devlopment we develop the website for the brands.</p>
+        </div>
+        </div>
+        </>
     )
 }
 
--- nextjs_app/components/Navbar.jsx
+++ nextjs_app/components/Navbar.jsx
@@ -1,13 +1,13 @@
-
+import style from './navbar.module.css'
 import Link from "next/link"
 export default function Navbar (){
     return(
-        <header className="flex justify-between p-4 items-center">
+        <header className={style.nav}>
             
             <div>
-                <h1 className="text-2xl">Ansh</h1>
+                <h1>Ansh</h1>
             </div>
-            <div className="flex gap-5 text-2xl m-6">
+            <div className={style.nav_menu} >
                <Link href="/"><p>Home</p></Link> 
                 <Link href="/about"><p>About</p></Link>
                 <Link href="/services"><p>Services</p></Link>
