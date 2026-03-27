87bb8c1739a94f546e8860daab29d97cae170cb2
Ansh Gupta <ng607301@gmail.com>
2026-03-27 17:15:11 +0530

    client component 7 api fetch

--- nextjs_app/app/(user)/page.js
+++ nextjs_app/app/(user)/page.js
@@ -1,5 +1,31 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
 const Home =()=>{
-  return <h1 className="font-roboto">hi i am learning next js</h1>
+  const [data, setData] = useState([])
+   const fetchData = async()=>{
+    try {
+      const response = await fetch("https://dummyjson.com/posts")
+      const result = await response.json()
+      setData(result.posts)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+   }
+   useEffect(()=>{
+    fetchData()
+   },[])
+  return <>
+  <h1 className="font-roboto">hi i am learning next js</h1>
+  <button className="bg-yellow-400" onClick={()=>alert("hi")}>Click me</button>
+  {
+    data.map((el)=>{
+      return <li>{el.title}</li>
+    })
+  }
+  </>
 }
 
 export default Home
